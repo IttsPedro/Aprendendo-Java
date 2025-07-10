e0f29dede09fc89972a4ec41346cf6408af68adb
IttsPedro <peterpereira204@gmail.com>
2025-07-10 20:57:44 +0100

    Aprendi a usar list, for, length e charAt

---
+++ src/Mentoria/ExercicioAccountList.java
@@ -0,0 +1,41 @@
+package Mentoria;
+
+import Exemplos_Capitulo3.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExercicioAccountList {
+
+    public static void main(String[] args)
+    {
+        List<Account> accounts = new ArrayList<>();
+
+        accounts.add(new Account("Pedro", 100.00));
+        accounts.add(new Account("Anna", 120.00));
+
+        // Usei for-each para mostrar o saldo das contas
+        for (Account account : accounts)
+        {
+            displayAccount(account);
+        }
+        Account anna = new Account("Anna", 80.00);
+        List<Account> count = List.of(new Account ("Pedro", 100),anna);
+    }
+
+
+    // Usei método display
+    public static void displayAccount(Account accountToDisplay)
+    {
+        System.out.printf("%s: R$%.2f %n", accountToDisplay.getName(), accountToDisplay.getBalance());
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/Mentoria/ExercicioMentoria.java
@@ -0,0 +1,21 @@
+package Mentoria;
+
+public class ExercicioMentoria {
+    public static void main(String[] args) {
+        String[] name = new String [4];
+        int lenght = name.length;
+        name [0] = "Pedro";
+        name [1] = "Anna";
+        name [2] = "Lucas";
+        name [3] = "Rogerio";
+
+        for (int i = 0; i < name.length ; i++) {
+            System.out.printf("%s%n", name[i]);
+        }
+
+    }
+}
+
+
+
+
