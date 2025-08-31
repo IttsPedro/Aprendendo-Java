97268d41b792ce0d723fa12147bfe4e580b2b47f
IttsPedro <peterpereira204@gmail.com>
2025-08-31 22:27:56 +0100

    Continuação dos exercícios do capitulo 5

--- src/Exemplos_Capitulo5/Interest.java
+++ src/Exemplos_Capitulo5/Interest.java
@@ -2,10 +2,9 @@ package Exemplos_Capitulo5;
 // Figura 5.6: Interest.java
 // Cálculos de juros compostos com for
 
-public class Interest
-{
-    public static void main(String[] args)
-    {
+public class Interest {
+    public static void main(String[] args) {
+
         double amount; // quantia em depósito ao fim de cada ano
         double principal = 1000.0; // quantidade inicial antes dos juros
         double rate = 0.05; // taxa de juros
@@ -14,8 +13,8 @@ public class Interest
         System.out.printf("%s%20s %n", "Year", "Amount on deposit");
 
         // calcula quantidade de depósito para cada um dos dez anos
-        for (int year = 1; year <= 10 ; ++year)
-        {
+        for (int year = 1; year <= 10; ++year) {
+
             // calcula nova quantidade durante ano específicado
             amount = principal * Math.pow(1.0 + rate, year);
 
